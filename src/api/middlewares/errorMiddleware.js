e5cee6edf06e1e191233c2daae042566e60c4fa0
Ludovic Sinquin <ludovic.sinquin@gmail.com>
2021-03-02 21:42:49 +0100

    Deleted a forgotten console.log()

--- src/api/middlewares/errorMiddleware.js
+++ src/api/middlewares/errorMiddleware.js
@@ -3,17 +3,14 @@ const ServerError = require("../../utils/errors/ServerError");
 
 module.exports = (error, req, res, next) => {
   if (error instanceof ServerError) {
-    console.log("bien attrapé");
     res
       .status(error.statusCode)
       .send({ error: error.errorId, message: error.message });
   } else {
     console.log(error);
-    res
-      .status(500)
-      .send({
-        error: "err_unknown",
-        message: "Une erreur innatendu a eu lieu",
-      });
+    res.status(500).send({
+      error: "err_unknown",
+      message: "Une erreur innatendu a eu lieu",
+    });
   }
 };
